dbdbcb91a468f94f16435c66169dfbd11280dbba
samuelfsd <samuel.estudo778@gmail.com>
2025-08-27 02:16:31 -0300

    fix: correct imports

--- src/main/kotlin/tech/jaya/ridely/domain/Passenger.kt
+++ src/main/kotlin/tech/jaya/ridely/domain/Passenger.kt
@@ -1,11 +1,6 @@
 package tech.jaya.ridely.domain
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
 @Entity
@@ -22,12 +17,11 @@ class Passenger(
     var email: String = "",
 
     @Column(name = "in_traveling", nullable = false)
-    var inTraveling : Boolean = false,
+    var inTraveling: Boolean = false,
 
     @Column(name = "activation_date", nullable = false)
     var activationDate: LocalDateTime = LocalDateTime.now(),
 ) {
-
 
 
 }
--- src/main/kotlin/tech/jaya/ridely/domain/Ride.kt
+++ src/main/kotlin/tech/jaya/ridely/domain/Ride.kt
@@ -1,17 +1,8 @@
 package tech.jaya.ridely.domain
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
-import tech.jaya.ridely.domain.exceptions.RideInvalidState
+import jakarta.persistence.*
 import tech.jaya.ridely.domain.enums.Status
+import tech.jaya.ridely.domain.exceptions.RideInvalidState
 import java.math.BigDecimal
 
 /**
